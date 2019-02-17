bc0038b877d212552e544abd299712bbe6b70781
vince <vince.buscarello@gmail.com>
2019-02-16 19:05:29 -0700

    store questions and answers in localstorage

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,17 @@
         </div>
       </li>
     </ul>
+    <br>
     <input v-model="newquestion" placeholder="Add question here!">
+    <br>
     <textarea v-model="newchoices" placeholder="Add multiple choice answers seperated by a comma"></textarea>
+    <br>
     <input v-model="newCorrectAnswer" placeholder="Add correct answer">
-    
+    <br>
     <button class="button btn-primary" @click="addQuestion">Add Question</button>
+    <br>
+    <br>
+    <button class="button btn-primary" @click="save">Save Progress</button>
   </div>
 </template>
 
@@ -47,9 +53,14 @@ export default {
       ]
     };
   },
+  mounted() {
+    localStorage.clear();
+  },
   methods: {
     addQuestion: function() {
+      var latestQuestionId = this.Questions[this.Questions.length - 1].id;
       var newQuestion = {
+        id: latestQuestionId + 1,
         question: this.newquestion,
         choices: makeChoices(this.newchoices),
         correctAnswer: this.newcorrectAnswer,
@@ -62,6 +73,20 @@ export default {
         return true;
       }
       return false;
+    },
+    save: function() {
+      this.Questions.map(q => {
+        localStorage.setItem(
+          q.id,
+          //json.parse to get me out!
+          JSON.stringify({
+            id: q.id,
+            question: q.question,
+            userAnswer: q.userAnswer,
+            correct: q.userAnswer == q.correctAnswer
+          })
+        );
+      });
     }
   }
 };
